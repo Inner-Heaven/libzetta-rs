31b7dee408d7c588e1269b8cff58ef282ab4c333
Andrey Cherkashin <with.out@me.com>
2019-01-19 17:21:33 -0800

    Fix. Parsing raided vdevs. Fixes #32

--- src/zpool/vdev.rs
+++ src/zpool/vdev.rs
@@ -47,6 +47,36 @@ impl Disk {
 /// Basic building block of
 /// [Zpool](https://www.freebsd.org/doc/handbook/zfs-term.html).
 #[derive(Debug, Clone, PartialEq, Eq)]
+pub(crate) enum VdevType {
+    /// Just a single disk or file.
+    Naked,
+    /// A mirror of multiple vdevs
+    Mirror,
+    /// ZFS implements [RAID-Z](https://blogs.oracle.com/ahl/what-is-raid-z), a
+    /// variation on standard RAID-5 that offers better distribution of
+    /// parity and eliminates the “RAID-5 write hole”.
+    RaidZ,
+    /// The same as RAID-Z, but with 2 parity drives.
+    RaidZ2,
+    /// The same as RAID-Z, but with 3 parity drives.
+    RaidZ3,
+}
+
+impl VdevType {
+    pub fn from_str(source: &str) -> VdevType {
+        match source {
+            "mirror" => VdevType::Mirror,
+            "raidz1" => VdevType::RaidZ,
+            "raidz2" => VdevType::RaidZ2,
+            "raidz3" => VdevType::RaidZ3,
+            _ => VdevType::Naked
+        }
+    }
+}
+
+/// Basic building block of
+/// [Zpool](https://www.freebsd.org/doc/handbook/zfs-term.html).
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Vdev {
     /// Just a single disk or file.
     Naked(Disk),
